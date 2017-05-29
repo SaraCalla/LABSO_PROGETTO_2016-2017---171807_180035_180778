f431b35a66dfdd7763cfc26e322ad28a6fa0bce0
calla95 <calla95@hotmail.it>
2017-05-29 20:19:19 +0200

    Commenti
    status serve?

--- definitivo/pmanager.c
+++ definitivo/pmanager.c
@@ -6,29 +6,35 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
-#include "caso.h"
+#include "pmanager.h"
 
+//Variabili utilizzate in pnew
 int global_variable=0;
-int status;
+//int status;		serve??
 
-int scrivi=1;
+int SCRIVI=1; 	//cambiare int con define??
 int LEGGI=0;
 
+
 /* FUNZIONI DI SUPPORTO*/
+
+/* 
+*Funzione che compara 2 array di caratteri
+*	-se sono uguali ritorna true
+*	-altrimenti false
+*/
 
 bool string_compare(char string1[],char string2[])
 {
 	int i=0;
 	for(; string1[i] != '\0' || string2[i]!='\0'; i++)
 	{	
-		//printf("%d\n",i);
-		//printf("string1= %c, string2=%c \n",string1[i],string2[i]);
 		if(string1[i]!=string2[i])
 		{
 		return false;
 		}
 	}
-return true;
+	return true;
 }
 
 
@@ -37,16 +43,20 @@ return true;
 
 void phelp()
 {
-	system("cat phelp.txt");
+	system("cat phelp.txt");		//system call per aprire il file "phelp.txt" contente le informazioni sui comandi 
 }
+
 
 
 void plist(int n)
 {
-	printf("n in plist: %d\n",n);	
 	printf(" Lista dei processi generati: \n");
+	
+	//stampa a video la lista di processi
 	for(int i=0;i<=n;i++)
 	{
+		//CONTROLLO DELLO STATO DEL PROCESSO 
+		//se un processo è attivo stampa a video le informazioni base
 		if(lista_processi[i].chiuso!=true)
 		{
 			printf("Processo %d ",i);
@@ -54,6 +64,8 @@ void plist(int n)
 			printf("pid= %d - ",lista_processi[i].pid);
 			printf("ppid= %d ; \n",lista_processi[i].ppid);	
 		}
+		
+		//se invece il processo è stato chiuso al posto delle informazioni apparirà una "x"
 		else
 		{
 			printf("Processo %d ",i);
@@ -68,23 +80,32 @@ void plist(int n)
 bool pnew(char nome[],int n)
 {	
 
-	printf("n in pnew: %d\n",n);
 	int val=3;
-	pid_t childPID; 
-	pipe(lista_processi[n].fd);
-	childPID = fork();
+	pid_t childPID;   		//dichiarazione del pid del figlio 
+	pipe(lista_processi[n].fd);	
+	childPID = fork();		//eseguo una fork e associo il pid creato a childPID
 	int comando=-1;
 	int buf=-1;
+	
+	//CONTROLLO DEL SUCCESSO DELLA FORK 
 	if (childPID >= 0)
-	{ // fork's success
+	{ 
+		
+		//CODICE PROCESSO FIGLIO
 		if (childPID == 0)
-        	{ // child process
+        	{ 
         		char byte=0;
 			global_variable++;
-			close(lista_processi[n].fd[scrivi]);
+			close(lista_processi[n].fd[SCRIVI]);
+			
+			//Informazioni stampate a video sul processo creato
 			printf("il pid del processo figlio è %d \n", getpid());
 			printf("e il processo del padre è %d \n",getppid());
+			
+			//DEB
 			printf("metto in pausa il figlio\n");
+			
+			
 			int comando;
 			comando=read(fd[0],&buf,100);
 			while(comando==0) //dormo fino a quando non do la condizione
@@ -128,24 +149,31 @@ bool pnew(char nome[],int n)
 			close(lista_processi[n].fd[LEGGI]);
 			printf("sono uscito dal while\n");
 			}
-			else 
-			{ // parent process
-				close(lista_processi[n].fd[LEGGI]);
-				write(lista_processi[n].fd[scrivi],&val;
-				
-				strcpy(lista_processi[n].nome_processo,nome);
-				lista_processi[n].pid=childPID;
-				lista_processi[n].ppid=getpid();
-				lista_processi[n].chiuso=false;
-				system("echo processo creato");		   		    
-        		};	
+		else 
+		{ // parent process
+			close(lista_processi[n].fd[LEGGI]);
+			write(lista_processi[n].fd[scrivi],&val;
+			
+			strcpy(lista_processi[n].nome_processo,nome);		//associo il nome dato dall'utente al processo
+			
+			 //inserimento del processo creto nella lista dei processi     
+			lista_processi[n].pid=childPID;
+			lista_processi[n].ppid=getpid();
+			lista_processi[n].chiuso=false;		//imposto chiuso a false per indicare che il processo è attivo 
+			system("echo processo creato");		//system call per ritornare al nostro terminale 		    
+       		};	
         }
+				      
+				      
+	//CASO IN CUI LA FORK NON HA AVUTO SUCCESSO
 	else 
-	{ // fork's failure
-		printf("\nFork failure!\n");
+	{ 
+		printf("\nLa Fork ha fallito!\n");
 		return false;
 	}
-	printf("alla fine di pnew n: %d\n",n);
+
+			      
+			      
 	return true;
 
 }
@@ -155,13 +183,17 @@ void process_info(int n, char nome[])
 {
 	for(int i=0; i<n; i++)
 	{
+		//CERCO IL PRCESSO INIDICATO DALL'UTENTE
 		if (string_compare(lista_processi[i].nome_processo, nome) )
 		{
+			//se il processo è ancora attivo stampo le sue informazioni
 			if(lista_processi[i].chiuso!=true)
 			{
 				printf("pid= %d - ",lista_processi[i].pid);
 				printf("ppid= %d ; \n",lista_processi[i].ppid);	
 			}
+			
+			//altrimenti se è stato chiuso al poste delle informazioni stampo "x"
 			else
 			{
 				printf("pid= x - ");
@@ -175,16 +207,14 @@ void process_info(int n, char nome[])
 bool chiudi_processo (int n,char nome[])
 {
 
-	for(int i=0; i<n; i++) //cerca il processo di nome nome 
+	for(int i=0; i<n; i++) //cerca il processo di nome "nome" 
 	{
-		if (string_compare(lista_processi[i].nome_processo, nome)) //processo trovato, KILL 
+		if (string_compare(lista_processi[i].nome_processo, nome)) 
 		{
 			
-			kill(lista_processi[i].pid,SIGKILL);
+			kill(lista_processi[i].pid,SIGKILL);		//system call che uccide il processo
 			//i figli saranno orfani
-			lista_processi[i].chiuso=true;			
-			//lista_processi[i].pid=0;
-			//lista_processi[i].ppid=0;
+			lista_processi[i].chiuso=true;			//il processo è stato chiuso
 			return true;
 		}
 	}
